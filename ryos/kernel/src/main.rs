5fae64118dde8bc345d77a34ca684538e5f73dd6
yuval-bar <yuvalbar2007@gmail.com>
2025-01-14 12:05:53 +0200

    added some basic output

--- ryos/kernel/src/main.rs
+++ ryos/kernel/src/main.rs
@@ -1,21 +1,46 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
-
+use bootloader_api::info::FrameBufferInfo;
+use conquer_once::spin::OnceCell;
+use bootloader_x86_64_common::logger::LockedLogger;
+use log::__private_api::log;
 
 bootloader_api::entry_point!(kernel_main);
+mod terminal;
 
 // ↓ this replaces the `_start` function ↓
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
-        for byte in framebuffer.buffer_mut() {
-            *byte = 0x90;
-        }
-    }
+    let frame_buffer_optional = &mut boot_info.framebuffer;
+
+    // free the wrapped framebuffer from the FFI-safe abstraction provided by bootloader_api
+    let frame_buffer_option = frame_buffer_optional.as_mut();
+
+    // unwrap the framebuffer
+    let frame_buffer_struct = frame_buffer_option.unwrap();
+
+    // extract the framebuffer info and, to satisfy the borrow checker, clone it
+    let frame_buffer_info = frame_buffer_struct.info().clone();
+
+    // get the framebuffer's mutable raw byte slice
+    let raw_frame_buffer = frame_buffer_struct.buffer_mut();
+
+    // finally, initialize the logger using the last two variables
+    init_logger(raw_frame_buffer, frame_buffer_info);
+    log::info!("test");
+
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+pub(crate) fn init_logger(buffer: &'static mut [u8], info: FrameBufferInfo) {
+    let logger = LOGGER.get_or_init(move || LockedLogger::new(buffer, info, true, false));
+    log::set_logger(logger).expect("Logger already set");
+    log::set_max_level(log::LevelFilter::Trace);
+    log::info!("Hello, Kernel Mode!");
 }
